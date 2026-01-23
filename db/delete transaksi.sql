2463306446fb1cee0c9da4cca11aaed2d4d5b272
Toni Simanjuntak <58927171+tonisimanjuntak@users.noreply.github.com>
2026-01-24 00:19:00 +0700

    Create delete transaksi.sql

---
+++ db/delete transaksi.sql
@@ -0,0 +1,43 @@
+
+DELETE FROM riwayatstok;
+DELETE FROM riwayataktifitas;
+DELETE FROM riwayathutangekspedisi;
+
+DELETE FROM jurnaldetail;
+DELETE FROM jurnal;
+DELETE FROM postingjurnal;
+
+DELETE FROM bonustarget;
+DELETE FROM bonuspenjualan;
+DELETE FROM bonuspenagihan;
+
+DELETE FROM suratjalanrincian;
+DELETE FROM suratjalandetail;
+DELETE FROM suratjalan;
+
+
+/* Penjualan */
+DELETE FROM penagihandetail;
+DELETE FROM penagihan;
+DELETE FROM returpenjualandetail;
+DELETE FROM returpenjualan;
+DELETE FROM piutangdetail;
+DELETE FROM piutang;
+DELETE FROM penjualankwitansi;
+DELETE FROM penjualandetail;
+DELETE FROM penjualan;
+
+
+DELETE FROM returpembeliandetail;
+DELETE FROM returpembelian;
+DELETE FROM hutangekspedisi;
+DELETE FROM hutangdetail;
+DELETE FROM hutang;
+DELETE FROM pembeliandetail;
+DELETE FROM pembelian;
+
+DELETE FROM pengeluarandetail;
+DELETE FROM pengeluaran;
+DELETE FROM penerimaandetail;
+DELETE FROM penerimaan;
+
